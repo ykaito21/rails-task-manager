cfdd03a429b9c92ea9cd8b9b2591e71e20f8cce6
Yuki Kaito <ykaito21@gmail.com>
2018-05-22 16:51:59 +0900

    Add CRUD with rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: 'tasks#index'
-  get '/', to: 'tasks#index', as: :index
+  root to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
