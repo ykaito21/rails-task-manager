09fa02a017408d0a3f55c129a16d0e3170a545d9
Yuki Kaito <ykaito21@gmail.com>
2018-05-23 09:06:37 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
   get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
+  patch 'tasks/', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
